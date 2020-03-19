e70d28b06bf6ec0fd472d1b5993b4a42c21e5b8b
jcdova <jcdova@gmail.com>
2020-03-19 16:11:51 -0700

    search routes

--- server.js
+++ server.js
@@ -5,21 +5,20 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-// require('dotenv').config()
-
-// const database = require('./keys').mongoURI;
-// const DATABASE_URL = require('./.env');
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true, useUnifiedTopology: true
@@ -27,9 +26,10 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongooose'))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
